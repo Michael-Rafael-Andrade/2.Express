48f6e427d414047538bd16d96aaa8ecedacb5ee0
Michael <michaelrafaeldeandrade@gmail.com>
2025-11-13 19:18:06 -0300

    criando o arquivo app.js
    criando o arquivo app.js

--- 1.Introducao/app.js
+++ 1.Introducao/app.js
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from 'express'; // importar o express
 
-const app = new express();
+const app = new express();  // Cria o objeto app recebendo um novo express
 const porta = 3000;
 
-const ola_mundo = function(req, res){
-    res.send('Olá Mundo!');
+const ola_mundo = function(req, res){ // passando como parametro a requisição e a resposta
+    res.send('Olá Mundo!');  // exibindo a mensagem 
 }
 
-app.get('/', ola_mundo);
+app.get('/', ola_mundo); // pegando a mensagem para ser exibida
 
 app.listen(porta, () => {
-    console.log("Servidor rodando no endereço http://127.0.0.1:3000\n");
+    console.log("Servidor rodando no endereço http://127.0.0.1:3000\n"); // conectando no servidor!
 });

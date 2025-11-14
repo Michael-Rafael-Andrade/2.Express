8f6d6ee9eb613b336a57203917a133b8b3c845dd
Michael <michaelrafaeldeandrade@gmail.com>
2025-11-14 19:36:52 -0300

    novo projeto Notas
    novo projeto Notas

---
+++ 3.Notas/model/modelo.js
@@ -0,0 +1,25 @@
+// Neste arquivo serão definios todos os modelos de dados da aplicação
+class nota {
+    constructor(chave, titulo, texto) {
+        this.chave = chave;
+        this.titulo = titulo;
+        this.texto = texto;
+    }
+    get chave() {
+        return this.chave;
+    }
+    get titulo(){
+        return this.titulo;
+    }
+    get texto(){
+        return this.texto;
+    }
+    set titulo(novoTitulo){
+        this.titulo = novoTitulo;
+    }
+    set texto(novoTexto){
+        this.texto = novoTexto;
+    }
+}
+
+module.exports = Nota

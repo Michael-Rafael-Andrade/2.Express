e8d27fcb2be877c0a3ccda0a6409e22210885dec
Michael <michaelrafaeldeandrade@gmail.com>
2025-11-15 00:23:12 -0300

    Criando o sobre
    Criando o sobre

--- 3.Notas/controller/controllerIndex.js
+++ 3.Notas/controller/controllerIndex.js
@@ -1,6 +1,7 @@
 // importação da classe que gerencia as notas na memória
 const notas = require('../model/notaMemoria.js');
 
+
 // cria e já exporta a função que será responsável pela tela principal
 exports.tela_principal = async function(req, res){
     // nota criada para teste
@@ -14,3 +15,12 @@ exports.tela_principal = async function(req, res){
     // renderiza o arquivo index.hbs, dentro da pasta view
     res.render('index', contexto);
 }
+
+exports.sobre = async function(req, res){
+    contexto = {
+        titulo_pagina: "Sobre o Aplicativo",
+    }
+    // renderiza o arquivo dentro da pasta view
+    res.render('sobre', contexto);
+}
+

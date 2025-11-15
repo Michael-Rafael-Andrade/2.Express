2436e161859f3d1eded1a9c33fd2ac434b0ca4aa
Michael <michaelrafaeldeandrade@gmail.com>
2025-11-15 12:52:09 -0300

    alterando rotas e exclusão
    alterando rotas e exclusão

--- 3.Notas/routes/rotasNota.js
+++ 3.Notas/routes/rotasNota.js
@@ -12,5 +12,13 @@ router.post('/cria', controllerNota.cria_post);
 // GET consulta Nota 
 router.get('/consulta/:chave_nota', controllerNota.consulta);
 
+// GET altera Nota
+router.get('/altera/:chave_nota', controllerNota.altera_get);
+
+// POST altera Nota
+router.post('/altera/:chave_nota', controllerNota.altera_post);
+
+// GET Exclui Nota
+router.get('/deleta/:chave_nota', controllerNota.deleta);
 
 module.exports = router;
